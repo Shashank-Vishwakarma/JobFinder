d2e94fa39aa84ddbbdfb535fce66a321a5463abb
Shashank <shashank78680@gmail.com>
2024-07-01 13:58:15 +0530

    login route working fine

--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -38,4 +38,34 @@ router.post('/register', async (req, res)=>{
     }
 });
 
+router.post('/login', async (req, res)=>{
+    try {
+        const { email, password } = req.body;
+        if(!email || !password) {
+            return res.status(400).json({ error: 'email and password are required' });
+        }
+
+        const user = await User.findOne({ email: email });
+        // check if user exists or not
+        if(!user) {
+            return res.status(400).json({ error: "User does not exist. Please create a new account." });
+        }
+
+        // compare password
+        const isPasswordMatch = await user.comparePassword(password);
+        if(!isPasswordMatch) {
+            return res.status(400).json({ error: "Password does not match. Please try again!" });
+        }
+        const payload = {
+            id: user.id
+        };
+        const token = generateJWT(payload);
+        res.status(200).json({
+            token: token
+        });
+    } catch(err) {
+        res.status(500).json({ error: `Internal server error : ${err}` });
+    }
+});
+
 module.exports = router;
