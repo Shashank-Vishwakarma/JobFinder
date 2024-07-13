fae8c575d96a68c76caa9fc628db6f9330e09b1d
Shashank <shashank78680@gmail.com>
2024-07-13 12:45:20 +0530

    apply for job working

--- Frontend/src/components/Application/Application.jsx
+++ Frontend/src/components/Application/Application.jsx
@@ -2,27 +2,60 @@ import React from "react";
 import { useState, useContext } from "react";
 import { AppContext } from "../../context/AppContext";
 import Input from "../utils/Input";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 function Application() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [coverLetter, setCoverLetter] = useState("");
-    const [phone, setPhone] = useState("");
+    const [phone, setPhone] = useState(0);
     const [address, setAddress] = useState("");
     const [resume, setResume] = useState(null);
+
+    const { id } = useParams();
+
+    const applyForJob = async (e)=>{
+        e.preventDefault();
+
+        // create form data
+        const data = new FormData();
+        data.append("name", name);
+        data.append("email", email);
+        data.append("phone", phone);
+        data.append("coverletter", coverLetter);
+        data.append("address", address);
+        data.append("resume", resume);
+        data.append("jobId", id);
+        
+        try {
+            const response = await axios.post(
+                'http://127.0.0.1:3000/api/v1/application/apply',
+                data,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    },
+                    withCredentials: true
+                }
+            );
+
+            console.log(response);
+        } catch(err) {
+            console.log("error in applying for job : ", err);
+        }
+    }
 
     const handleFileChange = (e) => {
         setResume(e.target.files[0]);
     }
-
-    const { isAuthorized, user } = useContext(AppContext);
 
     return (
         <>
             <h1 className="text-2xl font-bold text-center m-4 bg-sky-400 p-2 rounded-md">Application Form</h1>
             <section className="flex items-center justify-center m-4 p-2 rounded-md">
                 <div className="bg-slate-300 shadow-md rounded-md p-4">
-                    <form className="grid grid-cols-2 gap-3">
+                    <form className="grid grid-cols-2 gap-3" onSubmit={applyForJob}>
                         <Input
                             type="text"
                             name="name"
@@ -37,7 +70,7 @@ function Application() {
                             id="email"
                             label="Email"
                             value={email}
-                            onChange={(e) => setName(e.target.value)}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                         <Input
                             type="number"
@@ -53,7 +86,7 @@ function Application() {
                             id="address"
                             label="Address"
                             value={address}
-                            onChange={(e) => setPhone(e.target.value)}
+                            onChange={(e) => setAddress(e.target.value)}
                         />
                         <textarea
                             placeholder="Cover Letter"
@@ -74,7 +107,7 @@ function Application() {
                                 style={{ width: "100%" }}
                             />
                         </div>
-                        <button type="submit" className="w-40 h-10 bg-sky-500 rounded-md shadow-md font-bold text-white">
+                        <button type="submit" className="w-40 h-10 bg-sky-500 rounded-md shadow-md font-bold">
                             Send Application
                         </button>
                     </form>
